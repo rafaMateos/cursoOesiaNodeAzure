51f9455136ee55b8da7f261de130c9f8978fe531
Rafael Mateos <rafaelmateostorres@gmail.com>
2021-07-19 00:45:36 +0200

    hola

--- restNodeApi/src/routes/test.js
+++ restNodeApi/src/routes/test.js
@@ -1,12 +1,29 @@
 const { Router } = require('express');
 const router = Router();
 
+const personas = require('../data.json');
+
 router.get('/' , (request , response) =>{
-    response.json({"Nombre" : "Francisco"});
+    response.json({"Saludo" : "Hola"});
 });
 
 router.get('/test' , (request , response) =>{
-    response.json({"Prueba" : "Test"});
+    response.send(personas);
 });
+
+router.post('/test' , (request , response) =>{
+    const { name , age , phoneNumber } = request.body;
+    if(name && age && phoneNumber) {
+        const id = personas.length + 1;
+        const newPersona = {id , ...request.body};
+        personas.push(newPersona);
+        response.send("OK");
+    }else{
+        response.send('Wrong request');
+    }
+});
+
+
+
 
 module.exports = router;
